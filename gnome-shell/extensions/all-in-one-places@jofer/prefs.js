a04fd5d6f8672173ddc6fae4e187c06b55e710fc
jferrao <jferrao@ymail.com>
2012-06-24 14:28:37 +0200

    Code optimization on the Gnome Shell new preferences panel.

--- gnome-shell/extensions/all-in-one-places@jofer/prefs.js
+++ gnome-shell/extensions/all-in-one-places@jofer/prefs.js
@@ -2,124 +2,91 @@ const Gtk = imports.gi.Gtk;
 //const GObject = imports.gi.GObject;
 const Lang = imports.lang;
 
+const Gettext = imports.gettext;
+const _ = Gettext.gettext;
+
 const Extension = imports.misc.extensionUtils.getCurrentExtension();
 const Lib = Extension.imports.lib;
 //const options = Extension.imports.menu_items;
 
-const schema = "org.gnome.shell.extensions.AllInOnePlaces";
+
+
+const SCHEMA = "org.gnome.shell.extensions.AllInOnePlaces";
+
+const SETTINGS = Lib.getSettings(Extension, SCHEMA);;
+
+const PANEL_WIDGETS = [
+    {'type': 'switch', 'args': { 'key': 'left-panel-menu', 'label': _("Show icon on the left side of the panel") }},
+    {'type': 'switch', 'args': { 'key': 'show-panel-icon', 'label': _("Show panel icon") }},
+    {'type': 'slider', 'args': { 'key': 'panel-icon-size', 'label': _("Panel icon size"), 'min': 8, 'max': 46, 'step': 1, 'default': 14 }},
+    {'type': 'switch', 'args': { 'key': 'show-panel-text', 'label': _("Show text in panel") }},
+    {'type': 'entry', 'args': { 'key': 'panel-text', 'label': _("Panel text") }},
+    {'type': 'combo', 'args': { 'key': 'file-manager', 'label': _("File manager"), 'values': {'nautilus': 'Nautilus', 'thunar': 'Thunar', 'pcmanfm': 'PCManFM'} }},
+    {'type': 'entry', 'args': { 'key': 'connect-command', 'label': _("Application for the  \"Connect to...\" item") }},
+    {'type': 'entry', 'args': { 'key': 'search-command', 'label': _("Application for the \"Search\" item") }}
+]
+
+let MENU_WIDGETS = [
+    {'type': 'switch', 'args': { 'key': 'show-desktop-item', 'label': _("Show desktop item") }},
+    {'type': 'switch', 'args': { 'key': 'show-trash-item', 'label': _("Show trash item") }},
+    {'type': 'switch', 'args': { 'key': 'hide-empty-trash-item', 'label': _("Hide empty trash item") }},
+    {'type': 'switch', 'args': { 'key': 'show-bookmarks-section', 'label': _("Show bookmarks section") }},
+    {'type': 'switch', 'args': { 'key': 'collapse-bookmarks-section', 'label': _("Drop-down style bookmarks section") }},
+    {'type': 'switch', 'args': { 'key': 'show-filesystem-item', 'label': _("Show file system item") }},
+    {'type': 'switch', 'args': { 'key': 'show-devices-section', 'label': _("Show devices section") }},
+    {'type': 'switch', 'args': { 'key': 'collapse-devices-section', 'label': _("Drop-down style devices section") }},
+    {'type': 'switch', 'args': { 'key': 'show-network-section', 'label': _("Show network section") }},
+    {'type': 'switch', 'args': { 'key': 'collapse-network-section', 'label': _("Drop-down style network section") }},
+    {'type': 'switch', 'args': { 'key': 'show-search-item', 'label': _("Show search item") }},
+    {'type': 'switch', 'args': { 'key': 'show-documents-section', 'label': _("Show recent documents section") }},
+    {'type': 'slider', 'args': { 'key': 'max-documents-documents', 'label': _("Maximum number of recent documents"), 'min': 5, 'max': 25, 'step': 1, 'default': 10 }},
+    {'type': 'slider', 'args': { 'key': 'item-icon-size', 'label': _("Menu item icon size"), 'min': 8, 'max': 46, 'step': 1, 'default': 22 }},
+]
 
 
 
-let options;
-
-
-
-function init()
-{
-    options = Lib.getSettings(Extension, schema);
-}
+function init() {}
 
 function buildPrefsWidget()
 {
-
-    let frame = new Gtk.Box({ orientation: Gtk.Orientation.VERTICAL, border_width: 10 });
+    let frame = new Gtk.Box({ orientation: Gtk.Orientation.VERTICAL, border_width: 10, spacing: 10 });
     
     // Global tab
     let box_panel = new Gtk.Box({ orientation: Gtk.Orientation.VERTICAL, border_width: 10 });
+    for (i in PANEL_WIDGETS) {
+        let widget_method = PANEL_WIDGETS[i]['type'];
+        let widget = new widgets()[widget_method](PANEL_WIDGETS[i]['args']);
+        box_panel.pack_start(widget, false, false, 5);
+    }
 
-    let switch_left_panel = new widgets().switch('left-panel-menu', "Show icon in left side of panel");
-    box_panel.pack_start(switch_left_panel, false, false, 5);
-    
-    let switch_panel_icon = new widgets().switch('show-panel-icon', "Show panel icon");
-    box_panel.pack_start(switch_panel_icon, false, false, 5);
+    // Items tab
+    let box_items = new Gtk.Box({ orientation: Gtk.Orientation.VERTICAL, border_width: 10 });
+    for (i in MENU_WIDGETS) {
+        let widget_method = MENU_WIDGETS[i]['type'];
+        let widget = new widgets()[widget_method](MENU_WIDGETS[i]['args']);
+        box_items.pack_start(widget, false, false, 5);
+    }
 
-    let slider_panel_icon = new widgets().slider('panel-icon-size', "Panel icon size", 8, 46, 1, 14);
-    box_panel.add(slider_panel_icon);
+    // Build tabs
+    let tabs = new Gtk.Notebook();
+    tabs.set_scrollable(true);
+    tabs.set_size_request(100, 100);
+    tabs.append_page(box_panel, new Gtk.Label({ label: "Global" }));
+    tabs.append_page(box_items, new Gtk.Label({ label: "Menu items" }));
+    frame.pack_start(tabs, false, false, 0);
 
-    let switch_panel_text = new widgets().switch('show-panel-text', "Show panel text");
-    box_panel.pack_start(switch_panel_text, false, false, 5);
-
-    let entry_panel_text = new widgets().entry('panel-text', "Panel text");
-    box_panel.pack_start(entry_panel_text, false, false, 5);
-
-	box_panel.pack_start(new Gtk.HSeparator(), false, false, 10)
-    
-    let select_file_manager = new widgets().combo('file-manager', "File manager", {'nautilus': 'Nautilus', 'thunar': 'Thunar', 'pcmanfm': 'PCManFM'});
-    box_panel.pack_start(select_file_manager, false, false, 5);
-    
-    let entry_connect_command = new widgets().entry('connect-command', "Application for the  \"Connect to...\" item");
-    box_panel.pack_start(entry_connect_command, false, false, 5);
-    
-    let entry_search_command = new widgets().entry('search-command', "Application for the \"Search\" item");
-    box_panel.pack_start(entry_search_command, false, false, 5);
-
-	// Buttons box
+    // Buttons box
     let box_buttons = new Gtk.Box({ orientation: Gtk.Orientation.HORIZONTAL, border_width: 0 });
 
-	let button_default = new Gtk.Button({ label: "Restore default values" });
-	let button_image = new Gtk.Image();
-	button_image.set_from_stock(Gtk.STOCK_REFRESH, 3);
-	button_default.set_image(button_image);
+    let button_default = new Gtk.Button({ label: "Restore default values" });
+    let button_image = new Gtk.Image();
+    button_image.set_from_stock(Gtk.STOCK_REFRESH, 3);
+    button_default.set_image(button_image);
     //button_default.connect('clicked', restoreDefaultValues());
     box_buttons.pack_end(button_default, false, false, 0);
     
-    box_panel.pack_end(box_buttons, false, false, 0);
+    frame.pack_end(box_buttons, false, false, 0);
 
-	// Items tab
-    let box_items = new Gtk.Box({ orientation: Gtk.Orientation.VERTICAL, border_width: 10 });
-
-    let switch_desktop_item = new widgets().switch('show-desktop-item', "Show desktop item");
-    box_items.pack_start(switch_desktop_item, false, false, 5);
-
-    let switch_trash_item = new widgets().switch('show-trash-item', "Show trash item");
-    box_items.pack_start(switch_trash_item, false, false, 5);
-
-    let switch_hide_trash = new widgets().switch('hide-empty-trash-item', "Hide empty trash item");
-    box_items.pack_start(switch_hide_trash, false, false, 5);
-
-    let switch_bookmarks_section = new widgets().switch('show-bookmarks-section', "Show bookmarks section");
-    box_items.pack_start(switch_bookmarks_section, false, false, 5);
-
-    let switch_collapse_bookmarks = new widgets().switch('collapse-bookmarks-section', "Drop-down style bookmarks section");
-    box_items.pack_start(switch_collapse_bookmarks, false, false, 5);
-
-    let switch_fs_item = new widgets().switch('show-filesystem-item', "Show file system item");
-    box_items.pack_start(switch_fs_item, false, false, 5);
-
-    let switch_devices_section = new widgets().switch('show-devices-section', "Show devices section");
-    box_items.pack_start(switch_devices_section, false, false, 5);
-
-    let switch_collapse_devices = new widgets().switch('collapse-devices-section', "Drop-down style devices section");
-    box_items.pack_start(switch_collapse_devices, false, false, 5);
-
-    let switch_network_section = new widgets().switch('show-network-section', "Show network section");
-    box_items.pack_start(switch_network_section, false, false, 5);
-
-    let switch_collapse_network = new widgets().switch('collapse-network-section', "Drop-down style network section");
-    box_items.pack_start(switch_collapse_network, false, false, 5);
-
-    let switch_search_item = new widgets().switch('show-search-item', "Show search item");
-    box_items.pack_start(switch_search_item, false, false, 5);
-
-    let switch_documents_section = new widgets().switch('show-documents-section', "Show recent documents section");
-    box_items.pack_start(switch_documents_section, false, false, 5);
-
-    let slider_max_documents = new widgets().slider('max-documents-documents', "Maximum number of recent documents", 5, 25, 1, 10);
-    box_items.add(slider_max_documents);
-    
-    let slider_icon_size = new widgets().slider('item-icon-size', "Menu item icon size", 8, 46, 1, 22);
-    box_items.add(slider_icon_size);
-
-	let box_commands = new Gtk.Box({ orientation: Gtk.Orientation.VERTICAL, border_width: 10 });
-	
-	let tabs = new Gtk.Notebook();
-	tabs.set_scrollable(true);
-	tabs.set_size_request(100, 100);
-	tabs.append_page(box_panel, new Gtk.Label({ label: "Global" }));
-	tabs.append_page(box_items, new Gtk.Label({ label: "Menu items" }));
-	
-	frame.pack_start(tabs, false, false, 0);
-	
     frame.show_all();
     return frame;
 }
@@ -130,13 +97,17 @@ function widgets() {}
 
 widgets.prototype = {
 
-    switch: function(key, label_text)
+    switch: function(args)
     {
+        if (Object.keys(args).length != 2 || (args['key'] == undefined || args['label'] == undefined)) {
+            throw new Error("Incorrect arguments in widgets().switch() method: needs key and label.");
+        }
+        
         let box = new Gtk.Box({ orientation: Gtk.Orientation.HORIZONTAL });
-        let label = new Gtk.Label({ label: label_text, xalign: 0 });
-        let widget = new Gtk.Switch({ active: options.get_boolean(key) });
+        let label = new Gtk.Label({ label: args['label'], xalign: 0 });
+        let widget = new Gtk.Switch({ active: SETTINGS.get_boolean(args['key']) });
         widget.connect('notify::active', function(switch_widget) {
-            options.set_boolean(key, switch_widget.active);
+            SETTINGS.set_boolean(args['key'], switch_widget.active);
         });
 /*
         if (settings_bool[setting].help) {
@@ -149,58 +120,63 @@ widgets.prototype = {
         return box;
     },
     
-    entry: function(key, label_text)
+    entry: function(args)
     {
+        if (Object.keys(args).length != 2 || (args['key'] == undefined || args['label'] == undefined)) {
+            throw new Error("Incorrect arguments in widgets().entry() method: needs key and label.");
+        }
+        
         let box = new Gtk.Box({ orientation: Gtk.Orientation.HORIZONTAL });
-        let label = new Gtk.Label({ label: label_text, xalign: 0 });
+        let label = new Gtk.Label({ label: args['label'], xalign: 0 });
         let widget = new Gtk.Entry({ hexpand: true });
-        widget.set_text(options.get_string(key));
+        widget.set_text(SETTINGS.get_string(args['key']));
         widget.connect('changed', function(entry_widget) {
-            options.set_string(key, entry_widget.get_text());
+            SETTINGS.set_string(args['key'], entry_widget.get_text());
         });
-
         box.pack_start(label, true, true, 0);
         box.add(widget);
         return box;
     },
     
-    slider: function(key, label_text, min, max, step, def)
+    slider: function(args)
     {
+        if (Object.keys(args).length != 6 || (args['key'] == undefined || args['label'] == undefined || args['min'] == undefined || args['max'] == undefined || args['step'] == undefined || args['default'] == undefined)) {
+            throw new Error("Incorrect arguments in widgets().slider() method: needs key, label, min, max, step, default.");
+        }
+        
         let box = new Gtk.Box({ orientation: Gtk.Orientation.HORIZONTAL });
-        let label = new Gtk.Label({ label: label_text, xalign: 0 });
-        let widget = new Gtk.HScale.new_with_range(min, max, step);
-        widget.set_value(options.get_int(key));
-        widget.add_mark(def, Gtk.PositionType.BOTTOM, String(def));  
-        widget.add_mark(min, Gtk.PositionType.BOTTOM, String(min));  
-        widget.add_mark(max, Gtk.PositionType.BOTTOM, String(max));  
+        let label = new Gtk.Label({ label: args['label'], xalign: 0 });
+        let widget = new Gtk.HScale.new_with_range(args['min'], args['max'], args['step']);
+        widget.set_value(SETTINGS.get_int(args['key']));
+        //widget.add_mark(args['default'], Gtk.PositionType.BOTTOM, String(args['default']));  
         widget.set_size_request(200, -1);
         widget.connect('value_changed', function(slider_widget) {
-            options.set_int(key, slider_widget.get_value());
+            SETTINGS.set_int(args['key'], slider_widget.get_value());
         });
-
         box.pack_start(label, true, true, 0);
         box.add(widget);
         return box;
     },
     
-    combo: function(key, label_text, values)
+    combo: function(args)
     {
+        if (Object.keys(args).length != 3 || (args['key'] == undefined || args['label'] == undefined || args['values'] == undefined)) {
+            throw new Error("Incorrect arguments in widgets().entry() method: needs key, label and values.");
+        }
+        
         let box = new Gtk.Box({ orientation: Gtk.Orientation.HORIZONTAL });
-        let label = new Gtk.Label({ label: label_text, xalign: 0 });
-
+        let label = new Gtk.Label({ label: args['label'], xalign: 0 });
         let widget = new Gtk.ComboBoxText();
-        for (command in values) {
-			widget.append(command, values[command]);
-		}
-		
-		widget.set_active_id(options.get_string(key));
-		widget.connect('changed', function(combo_widget) {
-            options.set_string(key, combo_widget.get_active_id());
+        for (command in args['values']) {
+            widget.append(command, args['values'][command]);
+        }
+        widget.set_active_id(SETTINGS.get_string(args['key']));
+        widget.connect('changed', function(combo_widget) {
+            SETTINGS.set_string(args['key'], combo_widget.get_active_id());
         });
-		
         box.pack_start(label, true, true, 0);
         box.add(widget);
         return box;
-	}
-    
+    }
+
 }
